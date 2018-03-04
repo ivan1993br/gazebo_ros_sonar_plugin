649761a260eae83fe586e3f0e3fd38aea240ff63
Rômulo Cerqueira <romulogcerqueira@gmail.com>
2018-03-03 23:26:53 -0300

    store all vertices of an osg scene and convert them to texture

--- test/Reverberation_test2.cpp
+++ test/Reverberation_test2.cpp
@@ -2,6 +2,7 @@
 #include <osgViewer/Viewer>
 #include <osg/Geode>
 #include <osg/ShapeDrawable>
+#include <osg/Texture2D>
 #include <osg/TriangleFunctor>
 
 // C++ includes
@@ -10,8 +11,8 @@
 #include <normal_depth_map/ImageViewerCaptureTool.hpp>
 #include "TestHelper.hpp"
 
-#define SHADER_PATH_FRAG "normal_depth_map/shaders/reverberation.frag"
-#define SHADER_PATH_VERT "normal_depth_map/shaders/reverberation.vert"
+// #define SHADER_PATH_FRAG "normal_depth_map/shaders/reverberation.frag"
+// #define SHADER_PATH_VERT "normal_depth_map/shaders/reverberation.vert"
 
 #define BOOST_TEST_MODULE "DynamicCubeMap_test"
 #include <boost/test/unit_test.hpp>
@@ -38,24 +39,22 @@ struct CollectTriangles {
 
 
 class CollectTrianglesVisitor : public osg::NodeVisitor {
+
     public:
         CollectTrianglesVisitor() {
+            vertices = new osg::Vec3Array();
             setTraversalMode( osg::NodeVisitor::TRAVERSE_ALL_CHILDREN );
         }
 
         void apply( osg::Geode& geode ) {
             for ( unsigned int i = 0; i<geode.getNumDrawables(); ++i ) {
                 osg::TriangleFunctor<CollectTriangles> triangleCollector;
-
                 geode.getDrawable(i)->accept(triangleCollector);
-                std::cout << "Node: " << (i + 1) << ", Triangles: " << triangleCollector.verts->size() << std::endl;
-                // for (unsigned int j = 0; j < triangleCollector.verts->size(); j++) {
-                    // std::cout << "Index: " << i << "," << j << std::endl;
-                    // osg::Matrix& matrix = _matrixStack.back();
-                    // _vertices->push_back((*triangleCollector.verts)[j] * matrix);
-               // }
+                vertices->insert(vertices->end(), triangleCollector.verts->begin(), triangleCollector.verts->end());
+                // std::cout << "Node: " << i << " / Vertices: " << triangleCollector.verts->size() << std::endl;
             }
         }
+        osg::ref_ptr< osg::Vec3Array > vertices;
 };
 
 osg::ref_ptr<osg::Group> createScene() {
@@ -69,35 +68,46 @@ osg::ref_ptr<osg::Group> createScene() {
     osg::ref_ptr<osg::ShapeDrawable> shape;
 
     // sphere
-    // shape = new osg::ShapeDrawable(new osg::Sphere(osg::Vec3(-3.0f, 0.0f, 0.0f), radius));
-    // shape->setColor(osg::Vec4(1.0f, 0.0f, 0.0f, 1.0f));
-    // geode->addDrawable(shape.get());
+    shape = new osg::ShapeDrawable(new osg::Sphere(osg::Vec3(-3.0f, 0.0f, 0.0f), radius));
+    shape->setColor(osg::Vec4(1.0f, 0.0f, 0.0f, 1.0f));
+    geode->addDrawable(shape.get());
 
-    // // box
+    // box
     shape = new osg::ShapeDrawable(new osg::Box(osg::Vec3(0.0f, 0.0f, 3.0f), 2* radius));
     shape->setColor(osg::Vec4(0.0f, 1.0f, 0.0f, 1.0f));
     geode->addDrawable(shape.get());
-    //
-    // // cylinder
-    // shape = new osg::ShapeDrawable(new osg::Cylinder(osg::Vec3(3.0f, 0.0f, 0.0f), radius, height));
-    // shape->setColor(osg::Vec4(0.0f, 0.0f, 1.0f, 1.0f));
-    // geode->addDrawable(shape.get());
-    //
-    // // cone
-    // shape = new osg::ShapeDrawable(new osg::Cone(osg::Vec3(0.0f, 0.0f, -3.0f), radius, height));
-    // shape->setColor(osg::Vec4(0.0f, 1.0f, 1.0f, 1.0f));
-    // geode->addDrawable(shape.get());
+
+    // cylinder
+    shape = new osg::ShapeDrawable(new osg::Cylinder(osg::Vec3(3.0f, 0.0f, 0.0f), radius, height));
+    shape->setColor(osg::Vec4(0.0f, 0.0f, 1.0f, 1.0f));
+    geode->addDrawable(shape.get());
+
+    // cone
+    shape = new osg::ShapeDrawable(new osg::Cone(osg::Vec3(0.0f, 0.0f, -3.0f), radius, height));
+    shape->setColor(osg::Vec4(0.0f, 1.0f, 1.0f, 1.0f));
+    geode->addDrawable(shape.get());
 
     return scene;
 }
 
 BOOST_AUTO_TEST_CASE(reverberation2_testCase) {
-
+    // create scene with models
     osg::ref_ptr<osg::Group> scene = createScene();
 
+    // get all triangles' vertices from all scene models
     CollectTrianglesVisitor visitor;
     scene->accept(visitor);
 
+    // until OpenGL 4.3, arrays in GLSL must be fixed, compile-time size.
+    // In this case, the triangles' vertices are stored as texture and passed to shader.
+    osg::ref_ptr<osg::Image> image = new osg::Image;
+    image->setImage(visitor.vertices->size(), 1, 1, GL_RGBA8, GL_RGBA, GL_FLOAT, (unsigned char*) &visitor.vertices[0], osg::Image::NO_DELETE);
+    osg::ref_ptr<osg::Texture2D> texture = new osg::Texture2D;
+    texture->setImage(image);
+
+    // TODO: pass the texture to GLSL as uniform
+
+    // display the scene
     osgViewer::Viewer viewer;
     viewer.setSceneData(scene);
     viewer.setUpViewInWindow(0,0,600,600);
