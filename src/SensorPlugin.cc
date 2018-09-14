99360c5b0f469ad3f24369ffeeeb35e514003798
ivan1993br <ivan1993br@github.com>
2018-09-14 08:30:38 -0300

    Modification of main class to sonar

--- src/SensorPlugin.cc
+++ src/SensorPlugin.cc
@@ -1,41 +1,37 @@
 #include <functional>
 #include <boost/scoped_ptr.hpp>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+#include <math.h>
 #include "gazebo/rendering/ogre_gazebo.h"
->>>>>>> 3db1d56... Modification of main class to sonar
-=======
->>>>>>> 7c8862b... 2
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
 #include <ignition/math/Vector3.hh>
 #include <gazebo/rendering/rendering.hh>
 #include "gazebo_ros_sonar_plugin/Sonar.hh"
+#include <gazebo/sensors/sensors.hh>
+
 
 namespace gazebo
 {
-  class SonarVisual : public ModelPlugin
+  class SonarVisual : public SensorPlugin
   {
     public: rendering::ScenePtr scene;
     public: boost::shared_ptr<gazebo::rendering::Sonar> sonar;
-    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
+    public: void Load(sensors::SensorPtr _parent, sdf::ElementPtr _sdf)
     {
       // Store the pointer to the model
-      this->model = _parent;
+      this->sensor = _parent;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
->>>>>>> 3db1d56... Modification of main class to sonar
-=======
->>>>>>> 7c8862b... 2
       // Listen to the update event. This event is broadcast every
       // simulation iteration.
-      this->updateConnection = event::Events::ConnectPause(
-          std::bind(&SonarVisual::OnUpdate, this));
+      // this->updateConnection = event::Events::ConnectPause(
+      //     std::bind(&SonarVisual::OnUpdate, this));
+
+      this->updateConnection =  event::Events::ConnectRender(
+        std::bind(&SonarVisual::OnUpdate, this));
+      this->updatePostRender =  event::Events::ConnectPostRender(
+        std::bind(&SonarVisual::OnPostRender, this));
 
 
       if (rendering::RenderEngine::Instance()->GetRenderPathType() ==
@@ -45,7 +41,7 @@ namespace gazebo
         return;
       }
 
-      std::string worldName = this->model->GetWorld()->GetName();
+      std::string worldName = this->sensor->WorldName();
       this->scene = rendering::get_scene(worldName);
       // Get scene pointer
       gzwarn << rendering::RenderEngine::Instance()->GetSceneCount() << " Num Scenes" << std::endl;
@@ -60,11 +56,17 @@ namespace gazebo
       { 
         
         gzwarn << "Got Scene" << std::endl;
+        double hfov = M_PI/2;
         this->sonar=boost::shared_ptr<rendering::Sonar>(new rendering::Sonar("FirstSonar",this->scene,false)); 
         this->sonar->SetFarClip(100.0);
         this->sonar->Init();
         this->sonar->Load(_sdf);
         this->sonar->SetCameraCount(1);
+        this->sonar->SetRangeCount(100, 100);
+        this->sonar->SetHorzFOV(hfov);
+        this->sonar->SetCosHorzFOV(hfov);
+        this->sonar->SetRayCountRatio(2);
+        this->sonar->CreateMyCam();
         this->sonar->CreateLaserTexture("GPUTexture");
       }
 
@@ -73,17 +75,31 @@ namespace gazebo
     // Called by the world update start event
     public: void OnUpdate()
     {
+      this->sonar->RenderImpl();
       // Apply a small linear velocity to the model.
-      this->model->SetLinearVel(ignition::math::Vector3d(.3, 0, 0));
+      //this->model->SetLinearVel(ignition::math::Vector3d(.3, 0, 0));
+    }
+
+    // Called by the world update start event
+    public: void OnPostRender()
+    {
+      this->sonar->PostRender();
+      // gzwarn << rendering::RenderEngine::Instance()->GetSceneCount() << " Num Scenes - " << this->sensor->WorldName() << std::endl;
+      //rendering::RenderEngine::Instance()->RemoveScene(const std::string &_name);
+      // Apply a small linear velocity to the model.
+      //this->model->SetLinearVel(ignition::math::Vector3d(.3, 0, 0));
     }
 
     // Pointer to the model
-    private: physics::ModelPtr model;
+    private: sensors::SensorPtr sensor;
 
     // Pointer to the update event connection
     private: event::ConnectionPtr updateConnection;
+
+    // Pointer to the update event connection
+    private: event::ConnectionPtr updatePostRender;
   };
 
   // Register this plugin with the simulator
-  GZ_REGISTER_MODEL_PLUGIN(SonarVisual)
+  GZ_REGISTER_SENSOR_PLUGIN(SonarVisual)
 }
